c97e87f28596f7ecefee955bb809afbf478f6b2d
mattyw <gh@mattyw.net>
2017-04-13 21:59:50 +0100

    use ioref - not sure if best approach but it works

--- src/Main.hs
+++ src/Main.hs
@@ -3,14 +3,18 @@ module Main where
 import qualified Network.Socket as Sock
 import Network.Multicast
 import Control.Concurrent
+import Data.IORef
 import Control.Monad
+import Control.Concurrent.Chan
 import Happstack.Server (ServerPart, Response, nullConf, simpleHTTP, ok, nullDir, notFound)
 
-recv = Sock.withSocketsDo $ do
+recv :: Chan String -> IO ()
+recv ch = Sock.withSocketsDo $ do
     sock <- multicastReceiver "224.0.0.1" 9999
     let loop = do
         (msg, _, addr) <- Sock.recvFrom sock 1024
         print (msg, addr)
+        writeChan ch $ show addr
         loop in loop
 
 send = Sock.withSocketsDo $ do
@@ -21,14 +25,25 @@ send = Sock.withSocketsDo $ do
         threadDelay 10000000 --10s
         loop in loop
 
--- TODO how do I get data from recv into output here.
 homePage :: ServerPart String
 homePage = ok $ do "haspi is running"
 
-myServer = simpleHTTP nullConf $ msum
+myServer :: IORef [String] -> IO ()
+myServer ref = simpleHTTP nullConf $ msum
     [ homePage ]
 
+worker :: Chan String -> IORef [String] -> IO ()
+worker ch ref = forever (readChan ch >>= \value -> modifyIORef ref (value:))
+
+newRef = newIORef ([] :: [String])
+
 main = do
-    forkIO myServer
-    forkIO recv
+    nodes <- newRef
+    ch <- newChan
+    forkIO $ worker ch nodes
+    forkIO $ myServer nodes
+    forkIO $ recv ch
     send
+
+-- TODO The IO Ref needs to be a set
+-- TODO The IO Ref needs to be read and displayed in the web page output
