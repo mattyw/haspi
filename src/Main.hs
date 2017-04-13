7d60beb62cbdd5f30b58d8559390f3a3b793ac9a
mattyw <gh@mattyw.net>
2017-04-13 21:20:57 +0100

    using happstack

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,10 @@
+module Main where
+
 import qualified Network.Socket as Sock
 import Network.Multicast
 import Control.Concurrent
+import Control.Monad
+import Happstack.Server (ServerPart, Response, nullConf, simpleHTTP, ok, nullDir, notFound)
 
 recv = Sock.withSocketsDo $ do
     sock <- multicastReceiver "224.0.0.1" 9999
@@ -8,7 +12,6 @@ recv = Sock.withSocketsDo $ do
         (msg, _, addr) <- Sock.recvFrom sock 1024
         print (msg, addr)
         loop in loop
-
 
 send = Sock.withSocketsDo $ do
     (sock, addr) <- multicastSender "224.0.0.1" 9999
@@ -18,6 +21,14 @@ send = Sock.withSocketsDo $ do
         threadDelay 10000000 --10s
         loop in loop
 
+-- TODO how do I get data from recv into output here.
+homePage :: ServerPart String
+homePage = ok $ do "haspi is running"
+
+myServer = simpleHTTP nullConf $ msum
+    [ homePage ]
+
 main = do
+    forkIO myServer
     forkIO recv
     send
